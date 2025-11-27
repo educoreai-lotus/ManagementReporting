37cc1699ce6e48abb924a42abd2c843a04ad60f0
nadeenhalabi <nadinhalavy@gmail.com>
2025-11-27 10:24:00 +0200

    Chore: replace AI custom mic emoji with SVG icon

--- frontend/src/components/AICustom/AICustomPage.jsx
+++ frontend/src/components/AICustom/AICustomPage.jsx
@@ -325,11 +325,12 @@ const AICustomPage = () => {
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  className="h-4 w-4"
                   fill="currentColor"
+                  viewBox="0 0 24 24"
+                  className="h-5 w-5"
                 >
-                  <path d="M12 14a3 3 0 0 0 3-3V5a3 3 0 0 0-6 0v6a3 3 0 0 0 3 3zm5-3a1 1 0 1 0-2 0 3 3 0 0 1-6 0 1 1 0 1 0-2 0 5 5 0 0 0 4 4.9V18h-2a1 1 0 1 0 0 2h6a1 1 0 1 0 0-2h-2v-2.1A5 5 0 0 0 17 11z" />
+                  <path d="M12 14a3 3 0 0 0 3-3V5a3 3 0 1 0-6 0v6a3 3 0 0 0 3 3z" />
+                  <path d="M19 11a1 1 0 1 0-2 0 5 5 0 0 1-10 0 1 1 0 1 0-2 0 7 7 0 0 0 6 6.92V20H9a1 1 0 1 0 0 2h6a1 1 0 1 0 0-2h-2v-2.08A7 7 0 0 0 19 11z" />
                 </svg>
               </button>
             </div>
